928b6499a1c003ef9bd3903b7612990c003bbe6c
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-02-02 00:27:24 +0530

    redux toolkit configured

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/HomePage/Homepage";
+import LoginPage from "scenes/LoginPage/LoginPage";
+import ProfilePage from "scenes/ProfilePage/ProfilePage";
 
 function App() {
+  
+  
+  
   return (
     <div className="App">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage />}/>
+          <Route path="/home" element={<HomePage />}/>
+          <Route path="/profile/:userId" element={<ProfilePage />}/>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
