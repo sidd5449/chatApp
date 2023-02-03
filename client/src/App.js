9ce521cf86f8ecbe19a68c763967156eb053cd0f
sidd5449 <76845396+sidd5449@users.noreply.github.com>
2023-02-04 01:49:45 +0530

    styling config done

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,30 @@
 import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
-import HomePage from "scenes/HomePage/Homepage";
-import LoginPage from "scenes/LoginPage/LoginPage";
-import ProfilePage from "scenes/ProfilePage/ProfilePage";
+import HomePage from "./scenes/HomePage/HomePage";
+import LoginPage from "./scenes/LoginPage/LoginPage";
+import ProfilePage from "./scenes/ProfilePage/ProfilePage";
+import { useMemo } from "react";
+import { useSelector } from "react-redux";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import { createTheme } from "@mui/material/styles";
+import { themeSettings } from "theme";
+
 
 function App() {
-  
-  
-  
+  const mode = useSelector((state) => state.mode);
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+
+
   return (
     <div className="App">
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<LoginPage />}/>
-          <Route path="/home" element={<HomePage />}/>
-          <Route path="/profile/:userId" element={<ProfilePage />}/>
-        </Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+          <Routes>
+            <Route path="/" element={<LoginPage />} />
+            <Route path="/home" element={<HomePage />} />
+            <Route path="/profile/:userId" element={<ProfilePage />} />
+          </Routes>
+        </ThemeProvider>
       </BrowserRouter>
     </div>
   );
